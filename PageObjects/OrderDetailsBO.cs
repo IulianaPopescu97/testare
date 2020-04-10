29491c8c4b894804079c84148e21f9d50e5bf44f
Vornicu Marius <56607885+mariusvornicu@users.noreply.github.com>
2020-04-10 21:45:58 +0300

    Add files via upload

---
+++ PageObjects/OrderDetailsBO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.PageObjects
+{
+    public class OrderDetailsBO
+    {
+            public string TxtName { get; set; } = "test";
+            public string TxtCountry = "test";
+            public string TxtCity = "test";
+            public string TxtCreditCard = "Hawaii";
+            public string TxtMonth = "03";
+            public string TxtYear = "2000";
+        }
+    }
